09880f33b0b971d775768d2b4bf136accea63346
Gholam Alam <luqustra@gmail.com>
2025-06-18 11:49:28 +1000

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,7 @@ terraform {
     resource_group_name  = "PTutorial-RG"
     storage_account_name = "ptutorial123"
     container_name       = "ptstate"
-    key                  = "terraform.tfstate"   # The state file name, can be project specific
+    key                  = "terraform.tfstate"
+    subscription_id      = "f7ebe6c1-5970-43d1-83d7-2a3ffdf07b74"  # Check this!
   }
 }
